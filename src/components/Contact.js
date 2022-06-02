0ac217162e6bd823bd906b15f68975a1c316c1eb
Royce Wee <weeroyce@gmail.com>
2022-06-02 16:47:27 -0700

    final with resume link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ function Contact() {
                     <h1 className="text-center mb-5">Contact Me!</h1>
                     <a className="contact-link" href="mailto:weeroyce@gmail.com"><MdEmail/> weeroyce@gmail.com</a>
                     <a className="contact-link" href=""><FaPhone/> 778-325-0325</a>
-                    <a className="contact-link" href=""><MdOutlineWysiwyg/> Get my resume</a>
+                    <a className="contact-link" href="https://drive.google.com/file/d/16Nlf7HWGnnYuCLxXwdwHfWqgCT3YbvOG/view?usp=sharing"><MdOutlineWysiwyg/> Get my resume</a>
                     <Col className="d-flex justify-content-around">
                         {/* <a className="contact-link" href="https://github.com/LizardButterfly"><FaGithub/></a>
                         <a className="contact-link" href="https://www.linkedin.com/in/royce-jacob-wee-a043611a2/"><FaLinkedin/></a> */}
