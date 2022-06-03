0edcda426011b284262816e2280a0981d13f3cd1
Royce Wee <weeroyce@gmail.com>
2022-06-03 15:22:16 -0700

    test update, removed phone link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
                 <Col md={4} className='d-flex flex-column align-self-center'>
                     <h1 className="text-center mb-5">Contact Me!</h1>
                     <a className="contact-link" href="mailto:weeroyce@gmail.com"><MdEmail/> weeroyce@gmail.com</a>
-                    <a className="contact-link" href=""><FaPhone/> 778-325-0325</a>
+                    <a className="contact-link"><FaPhone/> 778-325-0325</a>
                     <a className="contact-link" href="https://drive.google.com/file/d/16Nlf7HWGnnYuCLxXwdwHfWqgCT3YbvOG/view?usp=sharing"><MdOutlineWysiwyg/> Get my resume</a>
                     <Col className="d-flex justify-content-around">
                         {/* <a className="contact-link" href="https://github.com/LizardButterfly"><FaGithub/></a>
